d1eeebcf0fe4ceab66b6201cc527f3130d9e41d8
yih <Yi.Huang@ehealth-china.com>
2018-05-14 13:51:41 +0800

    0512 data commited

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const config = {
 	accountType: 1, //1:Current, 2: Savings, 3: Transmission
 	frequency: 3, //0:Once Off, 1:Weekly, 2:Fortnightly, 3:Monthly, 4:First Working Day, 5:Last Working Day
 	transferFileSource: '/input/data.xlsx',
-	reportFilePath: '/history/report/(CSV_Download)_000000000015932_20180510135202_Output.csv',
-	transferFilePath: '/history/upload/15932_2N_0510194219.csv',
+	reportFilePath: '/history/report/(CSV_Download)_000000000015932_20180513060423_Output.csv',
+	transferFilePath: '/history/upload/15932_2N_0513115945.csv',
 	actionDate: 20180531
 }
 
