a6ce7e62869a3d6ea80e45df0e10ce93469fdbe3
yih <Yi.Huang@ehealth-china.com>
2018-05-25 23:46:56 +0800

    Data transfer for 20180525, twice

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ const config = {
     accountType: 1, //1:Current, 2: Savings, 3: Transmission
     frequency: 3, //0:Once Off, 1:Weekly, 2:Fortnightly, 3:Monthly, 4:First Working Day, 5:Last Working Day
     transferFileSource: '/input/data.xlsx',
-    reportFilePath: '/history/report/(CSV_Download)_000000000015932_20180517145738_Output.csv',
-    transferFilePath: '/history/upload/15932_2N_0517205056.csv',
+    reportFilePath: '/history/report/(CSV_Download)_000000000015932_20180525032853_Output.csv',
+    transferFilePath: '/history/upload/15932_2N_0525092521.csv',
     actionDate: 20180531,
     verifyUploaded: {
         actionDate: '20180430',
